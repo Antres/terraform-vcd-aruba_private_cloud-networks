0ece4d1c0c3009e6ad22c841d0d5fe41e2a3f85b
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-11 18:46:17 +0200

    Update and rename main.tf to network.tf

--- network.tf
+++ network.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_version          = "> 0.12.0"
-  experiments               = []
-}
-
-locals {
-  defaults_dns               = tolist(["8.8.8.8", "1.1.1.1"])
-  defaults_lease_time        = 3600
-  
-  #A named map of networks. local.network[<NETWORK_NAME>] => <NETWORK>
-  networks                  = zipmap(var.networks[*].name, var.networks)
-    
-  # A list of network names with attribute "routed" is setted to TRUE
-  routed                    = compact([for network in var.networks: network.routed ? network.name : ""])
-    
-  # A list of network names with attribute "routed" is setted to FALSE
-  isolated                  = compact([for network in var.networks: !network.routed ? network.name : ""])
-  
-  # A list of network names where DHCP feature is required
-  dhcp                      = compact([for network in var.networks: network.dhcp.enable ? network.name : ""])
-}
-    
 resource "vcd_network_routed" "roueted-dhcp" {
   for_each                  = setintersection(local.routed, local.dhcp)
     
