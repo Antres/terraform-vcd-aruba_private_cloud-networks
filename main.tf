10eb8222785ebd52b08a0be23b4db789b80520b5
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-11 18:46:34 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version          = "> 0.12.0"
+  experiments               = []
+}
+
+locals {
+  defaults_dns               = tolist(["8.8.8.8", "1.1.1.1"])
+  defaults_lease_time        = 3600
+  
+  #A named map of networks. local.network[<NETWORK_NAME>] => <NETWORK>
+  networks                  = zipmap(var.networks[*].name, var.networks)
+    
+  # A list of network names with attribute "routed" is setted to TRUE
+  routed                    = compact([for network in var.networks: network.routed ? network.name : ""])
+    
+  # A list of network names with attribute "routed" is setted to FALSE
+  isolated                  = compact([for network in var.networks: !network.routed ? network.name : ""])
+  
+  # A list of network names where DHCP feature is required
+  dhcp                      = compact([for network in var.networks: network.dhcp.enable ? network.name : ""])
+}
+    
