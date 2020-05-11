5c8fe8981a061ecc7015b9d78dfef79f9650e33d
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-11 12:30:35 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -11,7 +11,8 @@ variable "networks" {
     
     description             = string                                    # Free text decription
     
-    dhcp                    = object({                                  # If end_range is empty, dhcp will be ignored
+    dhcp                    = object({
+                                enable            = bool
                                 start_range       = string              # First address to assign. If empty second ip in network will be used
                                 end_range         = string              # Last address to assign
                                 dns               = set(string)         # A list of DNS address to assign (only 2 will be used)
