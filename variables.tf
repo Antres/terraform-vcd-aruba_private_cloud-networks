abfe8ba62a2c54d7dbedecaaa8435405be6fb468
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-11 11:29:03 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,10 +4,18 @@ variable "networks" {
   description               = "List of networks"
   
   type = list(object({
-    name                    = string
-    network                 = string # ex. 192.168.0.0/24
-    routed                  = bool
-    description             = string
+    name                    = string                                    # MUST BE unique
+    
+    network                 = string                                    # ex. 192.168.0.0/24
+    routed                  = bool                                      # If TRUE a resource vcd_network_routed will be used, otherwise vcd_network_isolated
+    
+    description             = string                                    # Free text decription
+    
+    dhcp                    = object({                                  # If end_range is empty, dhcp will be ignored
+                                start_range       = string              # First address to assign. If empty second ip in network will be used
+                                end_range         = string              # Last address to assign
+                                dns               = set(string)         # A list of DNS address to assign (only 2 will be used)
+                              })
   }))
   
   default                   = []
