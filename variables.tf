8325bf81c871d06e7cee0d4421ebfb7e05e6d7ff
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-11 09:21:58 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,13 @@
+#variable "region" {}
 
+variable "networks" {
+  description               = "List of networks"
+  
+  type = list(object({
+    name                    = string
+    network                 = string # ex. 192.168.0.0/24
+    routed                  = bool
+  }))
+  
+  default                   = []
+}
