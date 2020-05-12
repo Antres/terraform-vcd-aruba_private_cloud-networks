5b5787f84f9d350addc53e75876de48444538a8c
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-12 18:51:41 +0200

    Update egress.tf

--- egress.tf
+++ egress.tf
@@ -1,7 +1,7 @@
 resource "vcd_nsxv_snat" "nsxv-snat" {
   # If Gateway Edge is Advanced
   #count                     = var.region.edge.advanced ? length(local.egress) : 0
-  for_each                   = tomap(var.region.edge.advanced ? length(local.egress) : {})
+  for_each                   = tomap(var.region.edge.advanced ? local.egress : {})
   
     org                     = var.region.vdc.org
     vdc                     = var.region.vdc.name
