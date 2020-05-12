6f94cdb56eb9f7d6aaac8180cf8e59e9ac94d920
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-12 19:24:48 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ locals {
   defaults_dns               = tolist(["8.8.8.8", "1.1.1.1"])
   defaults_lease_time        = 3600
   
+  format_egress_uuidv5       = "CN=%s,CN=%s,CN=%s,OU=%s,DC=networks,DC=aruba_private_cloud,DC=terraform"
+  
   #A named map of networks. local.network[<NETWORK_NAME>] => <NETWORK>
   networks                  = zipmap(var.networks[*].name, var.networks)
     
@@ -22,6 +24,6 @@ locals {
   egress                    = { for item in flatten([ for network in var.networks: 
                                               [ for network_egress in network.egress: 
                                                 { name = network.name, egress = network_egress}]]):
-                                  uuidv5("x500", format("CN=%s,CN=%s,CN=%s,OU=%s,DC=networks,DC=aruba_private_cloud,DC=terraform", join(", ", item.egress.ports), item.egress.to, item.egress.with_address, item.name)) => item }
+                                  uuidv5("x500", format(local.format_egress_uuidv5, join(", ", item.egress.ports), item.egress.to, item.egress.with_address, item.name)) => item }
 }
     
