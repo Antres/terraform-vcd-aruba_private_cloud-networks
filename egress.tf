5e8b3c8135f81d2348e3d829f449c196297f3702
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-12 19:14:42 +0200

    Update egress.tf

--- egress.tf
+++ egress.tf
@@ -1,6 +1,5 @@
 resource "vcd_nsxv_snat" "nsxv-snat" {
   # If Gateway Edge is Advanced
-  #count                     = var.region.edge.advanced ? length(local.egress) : 0
   for_each                   = tomap(var.region.edge.advanced ? local.egress : {})
   
     org                     = var.region.vdc.org
@@ -16,4 +15,29 @@ resource "vcd_nsxv_snat" "nsxv-snat" {
     translated_address      = coalesce(each.value.egress.with_address, var.region.edge.default_external_network_ip)
   
     depends_on = [ vcd_network_routed.roueted-dhcp, vcd_network_routed.roueted-nodhcp ]
+}
+
+resource "vcd_nsxv_firewall_rule" "nsxv-fw" {
+  for_each                  = tomap(var.region.edge.advanced ? local.egress : {})
+  
+    org                     = var.region.vdc.org
+    vdc                     = var.region.vdc.name
+    edge_gateway            = var.region.edge.name
+
+  source {
+    org_networks            = toset([each.value.name])
+  }
+
+  destination {
+    ip_addresses            = toset([each.value.egress.to])
+  }
+
+  dynamic "service" {
+    for_each                = toset(each.value.egress.ports)
+    
+    content {
+      port                  = split("/", service.value)[0]
+      protocol              = split("/", service.value)[1]
+    }
+  }
 }
