24c5342e606de073e1bf34c46c651910ce4794e1
Alberto Clemente <albertoclemente1983@gmail.com>
2020-05-12 18:50:05 +0200

    Update egress.tf

--- egress.tf
+++ egress.tf
@@ -1,18 +1,19 @@
 resource "vcd_nsxv_snat" "nsxv-snat" {
   # If Gateway Edge is Advanced
-  count                     = var.region.edge.advanced ? length(local.egress) : 0
+  #count                     = var.region.edge.advanced ? length(local.egress) : 0
+  for_each                   = tomap(var.region.edge.advanced ? length(local.egress) : {})
   
     org                     = var.region.vdc.org
     vdc                     = var.region.vdc.name
     edge_gateway            = var.region.edge.name
   
-    description             = "Egress SNAT rule ${local.egress[count.index].name}"
+    description             = "Egress SNAT rule ${local.egress[each.value.name].name}"
   
     network_type            = "ext"
     network_name            = tolist(var.region.edge.external_network)[0].name
 
-    original_address        = local.networks[local.egress[count.index].name].network
-    translated_address      = coalesce(local.egress[count.index].egress.with_address, var.region.edge.default_external_network_ip)
+    original_address        = local.networks[each.value.name].network
+    translated_address      = coalesce(each.value.egress.with_address, var.region.edge.default_external_network_ip)
   
     depends_on = [ vcd_network_routed.roueted-dhcp, vcd_network_routed.roueted-nodhcp ]
 }
